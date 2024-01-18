af2f38a1f3cc5daa54022c7583d7ce0f1d4bdbb0
AndrewCLu <andrewclu98@gmail.com>
2024-01-17 22:55:05 -0500

    add signing functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,127 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useState, ChangeEvent, useEffect } from "react";
+// @ts-ignore
+import { execHaloCmdWeb } from "@arx-research/libhalo/api/web.js";
 
-const inter = Inter({ subsets: ['latin'] })
+const App: React.FC = () => {
+  const [inputText, setInputText] = useState<string>("");
+  const [hashedText, setHashedText] = useState<string>("");
+  const [publicKey, setPublicKey] = useState<string>("");
+  const [signature, setSignature] = useState<string>("");
 
-export default function Home() {
+  useEffect(() => {
+    const computeHash = async (text: string) => {
+      if (text) {
+        const hash = await utf8ToSha256(text);
+        setHashedText(hash);
+      } else {
+        setHashedText("");
+      }
+    };
+
+    computeHash(inputText);
+  }, [inputText]);
+
+  const handleTextChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setInputText(event.target.value);
+  };
+
+  const utf8ToSha256 = async (text: string): Promise<string> => {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(text);
+    const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray
+      .map((b) => b.toString(16).padStart(2, "0"))
+      .join("");
+    return hashHex;
+  };
+
+  const copyToClipboard = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      alert("Copied to clipboard!");
+    } catch (err) {
+      alert("Failed to copy!");
+    }
+  };
+
+  const handleNfcSign = async () => {
+    const digest = await utf8ToSha256(inputText);
+    let command = {
+      name: "sign",
+      keyNo: 1,
+      digest,
+    };
+
+    let res;
+    try {
+      res = await execHaloCmdWeb(command);
+      setPublicKey(res.publicKey);
+      setSignature(res.signature.der);
+    } catch (e) {
+      alert("Error: " + String(e));
+    }
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-200">
+      <h1 className="text-3xl font-bold text-gray-700 mb-4">
+        NFC Content Signer
+      </h1>
+      <p className="text-l text-black mb-4">
+        Digitally sign a piece of text with your FIDO2 compliant NFC card. Text
+        is UTF-8 encoded and SHA-256 hashed. Must be used on mobile browser.
+      </p>
+      <textarea
+        className="w-1/2 p-2 border-2 border-gray-400 rounded-lg mb-4 text-black h-1/4"
+        value={inputText}
+        onChange={handleTextChange}
+        placeholder="Paste your text here"
+      />
+      {hashedText && (
+        <div className="mt-4 p-3 border border-gray-400 rounded-lg bg-gray-100">
+          <h2 className="text-lg font-semibold text-gray-700">Text Hash</h2>
+          <p className="text-gray-600">{hashedText}</p>
+          <button
+            className="mt-2 px-4 py-1 bg-blue-300 text-white rounded hover:bg-blue-400 transition-colors flex items-center"
+            onClick={() => copyToClipboard(hashedText)}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            📋 Copy Hash
+          </button>
         </div>
-      </div>
+      )}
+      <button
+        className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition-colors"
+        onClick={handleNfcSign}
+      >
+        Sign with NFC Card
+      </button>
+      {publicKey && (
+        <div className="mt-4 p-3 border border-gray-400 rounded-lg bg-gray-100">
+          <h2 className="text-lg font-semibold text-gray-700">Public Key</h2>
+          <p className="text-gray-600">{publicKey}</p>
+          <button
+            className="mt-2 px-4 py-1 bg-blue-300 text-white rounded hover:bg-blue-400 transition-colors flex items-center"
+            onClick={() => copyToClipboard(publicKey)}
+          >
+            📋 Copy Public Key
+          </button>
+        </div>
+      )}
+      {signature && (
+        <div className="mt-4 p-3 border border-gray-400 rounded-lg bg-gray-100">
+          <h2 className="text-lg font-semibold text-gray-700">Signature</h2>
+          <p className="text-gray-600">{signature}</p>
+          <button
+            className="mt-2 px-4 py-1 bg-blue-300 text-white rounded hover:bg-blue-400 transition-colors flex items-center"
+            onClick={() => copyToClipboard(signature)}
+          >
+            📋 Copy Signature
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default App;
